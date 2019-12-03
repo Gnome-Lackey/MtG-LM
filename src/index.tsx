31a36e0598f5359803fa96e4a332df2f7280c6a3
Matthew_Pfister <matthew.pfister@slalom.com>
2019-12-03 13:33:50 -0600

    Only using hot reload app when we are running locally

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,12 @@ import * as serviceWorker from "serviceWorker.ts";
 
 import Routes from "components/Routes";
 
-const node = document.getElementById("root");
-const HotReloadableApp = hot(module)(Routes);
+const { ENV } = process.env;
 
-render(<HotReloadableApp />, node);
+const node = document.getElementById("root");
+const App = ENV === "local" ? hot(module)(Routes) : Routes;
+
+render(<App />, node);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
