c0bf935e15f22cad239f8db95a439414e9562f75
Matthew_Pfister <matthew.pfister@slalom.com>
2019-12-17 20:12:16 -0600

    fixing season create node mapper

--- src/mappers/seasons.ts
+++ src/mappers/seasons.ts
@@ -1,13 +1,22 @@
 import { SeasonFields } from "components/Hooks/useFormData/models/FormFields";
 import { CreateSeasonNode, UpdateSeasonNode } from "services/models/Nodes";
 
-export const toCreateNode = (details: SeasonFields): CreateSeasonNode => ({
-  startedOn: details.startedDate,
-  endedOn: details.endedDate,
-  players: details.players.length ? details.players.map((player) => player.key) : [],
-  set: details.set.key,
-  isActive: details.isActive || false
-});
+export const toCreateNode = (details: SeasonFields): CreateSeasonNode => {
+  const { startedDate, endedDate, players, set, isActive } = details;
+
+  const node: CreateSeasonNode = {
+    startedOn: startedDate,
+    set: set.key,
+    players: players.length ? players.map((player) => player.key) : [],
+    isActive: isActive || false
+  };
+
+  if (endedDate) {
+    node.endedOn = endedDate;
+  }
+
+  return node;
+};
 
 export const toUpdateNode = (details: SeasonFields): UpdateSeasonNode => {
   const { startedDate, endedDate, players, set, isActive } = details;
