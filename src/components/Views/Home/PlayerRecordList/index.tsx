558e970c9542212c08521be94932aef1af1a24a1
Matthew_Pfister <matthew.pfister@slalom.com>
2020-02-03 14:19:36 -0600

    making playerrecordlist easier to read

--- src/components/Views/Home/PlayerRecordList/index.tsx
+++ src/components/Views/Home/PlayerRecordList/index.tsx
@@ -16,52 +16,55 @@ interface PlayerRecordListProps {
   user: User;
 }
 
+const renderContent = (
+  isRequestLoading: boolean,
+  hasSeason: boolean,
+  players: Player[],
+  user: User
+): JSX.Element | JSX.Element[] => {
+  if (isRequestLoading) {
+    return (
+      <li className="record-spinner">
+        <Spinner inline />
+      </li>
+    );
+  } else if (!hasSeason) {
+    return (
+      <li className="record-empty-message">
+        There are no active seasons. Please contact an admin to get a season started.
+      </li>
+    );
+  } else if (players.length) {
+    return players.map((player) => (
+      <PlayerRecordListItem key={player.id} player={player} userId={user && user.id} />
+    ));
+  }
+
+  return <li className="record-empty-message">There are no players in this season.</li>;
+};
+
 const PlayerRecordList: React.FunctionComponent<PlayerRecordListProps> = ({
   hasSeason,
   isRequestLoading,
   players,
   showWarning,
   user
-}: PlayerRecordListProps): React.FunctionComponentElement<PlayerRecordListProps> => {
-  let content: JSX.Element | JSX.Element[] = (
-    <li className="record-empty-message">There are no players in this season.</li>
-  );
-
-  if (isRequestLoading) {
-    content = (
-      <li className="record-spinner">
-        <Spinner inline />
+}: PlayerRecordListProps): React.FunctionComponentElement<PlayerRecordListProps> => (
+  <ul className="player-record-list">
+    <li className="record-headers">
+      <p className="header name">Player Name</p>
+      <p className="header wins">Total Wins</p>
+      <p className="header losses">Total Losses</p>
+    </li>
+    {showWarning ? (
+      <li>
+        <p className="not-in-season-message">
+          You are currently not in this season. Please contact an admin to be added.
+        </p>
       </li>
-    );
-  } else if (!hasSeason) {
-    content = (
-      <li className="record-empty-message">
-        There are no active seasons. Please contact an admin to get a season started.
-      </li>
-    );
-  } else if (players.length) {
-    content = players.map((player) => (
-      <PlayerRecordListItem key={player.id} player={player} userId={user && user.id} />
-    ));
-  }
-
-  return (
-    <ul className="player-record-list">
-      <li className="record-headers">
-        <p className="header name">Player Name</p>
-        <p className="header wins">Total Wins</p>
-        <p className="header losses">Total Losses</p>
-      </li>
-      {showWarning ? (
-        <li>
-          <p className="not-in-season-message">
-            You are currently not in this season. Please contact an admin to be added.
-          </p>
-        </li>
-      ) : null}
-      {content}
-    </ul>
-  );
-};
+    ) : null}
+    {renderContent(hasSeason, isRequestLoading, players, user)}
+  </ul>
+);
 
 export default PlayerRecordList;
